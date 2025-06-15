6e8db81e37db2cc7777da4eb79c914eb9acf21d4
IanVarelaUNAB <ian.varela.cole@gmail.com>
2025-06-15 20:43:28 -0300

    Update firebase.ts

--- src/configuracion/firebase.ts
+++ src/configuracion/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const configuracionFirebase = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyBZoHfOXB2RzCtqOTGq0Ru4QYnVCSRuLns",
+  authDomain: "destination-wedding-dedf1.firebaseapp.com",
+  projectId: "destination-wedding-dedf1",
+  storageBucket: "destination-wedding-dedf1.firebasestorage.app",
+  messagingSenderId: "82813215875",
+  appId: "1:82813215875:web:cd47d0dc0e03129db267b6",
+  measurementId: "G-ZBKZ8L0PCR"
 };
 
 const app = initializeApp(configuracionFirebase);
